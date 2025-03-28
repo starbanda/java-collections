ac931526296656790d729eda431fd2b42068392d
VipulArora_2003 <vipularora9930@gmail.com>
2025-03-28 15:39:22 +0530

    [Vipul] ADD: Done with mapInterface

---
+++ src/main/java/mapInterface/FindMaxValueKey.java
@@ -0,0 +1,30 @@
+package com.collection.mapInterface;
+
+import java.util.*;
+
+public class FindMaxValueKey {
+    public static String findKeyWithMaxValue(Map<String, Integer> map) {
+        String maxKey = null;
+        int maxValue = Integer.MIN_VALUE;
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxValue) {
+                maxValue = entry.getValue();
+                maxKey = entry.getKey();
+            }
+        }
+
+        return maxKey;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 10);
+        map.put("B", 20);
+        map.put("C", 15);
+
+        String maxKey = findKeyWithMaxValue(map);
+
+        System.out.println("Key with the highest value: " + maxKey);  // Output: B
+    }
+}
